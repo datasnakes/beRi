ac81c2390a3e69af5dcf0339849f5def4413548d
RAG-home <rgilmore@umc.edu>
2018-04-27 01:46:59 -0500

    Added updated concept for the install command with main group.

--- beri/beRi.py
+++ beri/beRi.py
@@ -1,8 +1,14 @@
 import click
 
 
-@click.command()
+@click.group()
 @click.option("--install", nargs=-1)
+@click.option("--global")
 def beri(install):
     packages = list(install)
+
+
+@beri.command()
+@beri.option("packages")
+def install():
     pass
